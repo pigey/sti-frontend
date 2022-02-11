4ca121ff999a431e98c78578a18efa0287eaee2b
pigey <pigfof@gmail.com>
2022-02-11 11:52:07 +0100

    try 3

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use('/healthcheck', require('./routes/healthcheck.routes'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(express.static('public'))
+
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + "/public/index.html")

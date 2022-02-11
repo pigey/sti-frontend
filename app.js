0174f2c9c8b4ff8587eca54f6048f229451c1c0a
pigey <pigfof@gmail.com>
2022-02-11 14:51:21 +0100

    hejsan

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
 const express = require("express")
-var favicon = require ('serve-favicon')
-var path = require('path')
-const res = require("express/lib/response")
-
-
-const PORT = process.env.PORT || 3000
-
-const app = express()
-app.use('/healthcheck', require('./routes/healthcheck.routes'));
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.use(express.static('public'))
-
-
-app.get('/', function(req, res){
-    res.sendFile(__dirname + "/public/index.html")
-})
-
-app.listen(PORT, function(){
-    console.log(`Server started on port ${PORT}`)
-});
+var favicon = require('serve-favicon')
+var path = require('path')  
+const PORT = process.env.PORT || 3000  
+const app = express() 
+app.use('/healthcheck', require('./routes/healthcheck.routes')); 
+app.use(express.static('public'))  
+app.get('/', function(req, res){   res.sendFile(__dirname + "/public/index.html") }) 
+ app.listen(PORT, function(){  console.log(`Server started on port ${PORT}`) })

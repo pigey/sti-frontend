72038c23e5f2ad617adbc2860cfda01a6e1083f8
pigey <pigfof@gmail.com>
2022-02-11 13:31:53 +0100

    try 343

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express")
-var favicon = require ("serve-favicon")
-var path = require("path")
+var favicon = require ('serve-favicon')
+var path = require('path')
 
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use('/healthcheck', require('./routes/healthcheck.routes'));
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(express.static('public'))
 
 
@@ -16,5 +16,5 @@ app.get("/", function(req, res){
 })
 
 app.listen(PORT, function(){
-    console.log("Server started on port ${PORT}")
+    console.log(`Server started on port ${PORT}`)
 });

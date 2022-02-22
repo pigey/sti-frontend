bb48d711013580b05a0a13ed20a551a4b3ed4f6a
pigey <pigfof@gmail.com>
2022-02-22 10:23:39 +0100

    leader board start

--- public/js/index.js
+++ public/js/index.js
@@ -32,3 +32,43 @@ var checkdead = setInterval(function(){
         clearInterval(myInterval);
     }
 },10)
+
+
+
+
+
+
+
+
+    console.log("Running age")
+    var xhr = new XMLHttpRequest()
+    xhr.open("GET", "/js/scores.json")
+    xhr.onload = function(){
+        var data = JSON.parse(this.response)
+        createTable(data)
+    }
+    xhr.send()
+
+
+function createTable(data){
+    var appElement = document.getElementById("leader")
+    console.log(appElement)
+    var aTable = document.createElement("table")
+    appElement.appendChild(aTable)
+    aTable.appendChild(createRow(data[0].name, data[0].time))
+    aTable.appendChild(createRow(data[1].name, data[1].time))
+    aTable.appendChild(createRow(data[2].name, data[2].time))
+    aTable.appendChild(createRow(data[3].name, data[3].time))
+    console.log(aTable)
+}
+function createRow(name, time){
+    var aRow = document.createElement("tr")
+    aRow.appendChild(createCell(name))
+    aRow.appendChild(createCell(time))
+    return aRow
+}
+function createCell(content){
+    var aCell = document.createElement("td")
+    aCell.innerHTML = content
+    return aCell
+}
